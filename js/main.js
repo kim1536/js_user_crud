0e747d4e9eec3c2a2cec0735a00ca6f877a13f60
김권일 <kwonil.kim@gcnyy.com>
2022-03-21 15:40:03 +0900

    상세패이지 추가

--- js/main.js
+++ js/main.js
@@ -4,22 +4,24 @@ function getTopics(){
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://localhost:8080/topic', true);
 xhr.onload = () => {
-    if(xhr.status == 200 && xhr.readyState == xhr.DONE) {
-        const topics = JSON.parse(xhr.response); // JSON으로 데이터 가져오기
-// 결과로 구해진 topic 목록을 화면에 출력한다.
+    // JSON으로 데이터 가져오기
+    const topics = JSON.parse(xhr.response); 
+    if(xhr.status == 200 && xhr.readyState == 4) {
+
+        // 결과로 구해진 topic 목록을 화면에 출력한다.
         for(let i in topics){
-        var table = document.getElementById("topicsTable").getElementsByTagName('tbody')[0];
-        var newRow = table.insertRow(table.length);
+        let table = document.getElementById("topicsTable").getElementsByTagName('tbody')[0];
+        let newRow = table.insertRow(table.length);
         cell1 = newRow.insertCell(0);
         cell1.innerHTML = topics[i].id;
         cell2 = newRow.insertCell(1);
-        cell2.innerHTML = topics[i].title;
+        cell2.innerHTML = `<a style="cursor:pointer;" id="detailTopic" href="./detail.html?id=${topics[i].id}">${topics[i].title}</a>`;
         cell3 = newRow.insertCell(2);
         cell3.innerHTML = topics[i].age;
         cell4 = newRow.insertCell(3);
         cell4.innerHTML = topics[i].createAt;
         cell5 = newRow.insertCell(4);
-        cell5.innerHTML = `<button type="button" onClick="onEdit(this)">수정</button>
+        cell5.innerHTML = `<button type="button" onClick="onEdit(${topics[i].id})">수정</button>
                            <button type="button" onClick="onDelete(${topics[i].id})">삭제</button>`;
         
       }
@@ -30,10 +32,18 @@ xhr.onload = () => {
 xhr.send();
 }
 
+// 파라미터로 받은 id을 서버에 전달하여 id에 해당되는 topic을 화면에 출력한다.
+function onDetail(topicId) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://localhost:8080/topic/${topicId}`, true);
+}
+
+
+// 파라미터로 받은 id을 서버에 전달하여 id에 해당되는 topic을 삭제한다.
 function onDelete(topicId) {
     const xhr = new XMLHttpRequest();
     xhr.open('DELETE', `http://localhost:8080/topic/${topicId}`, true);
-    if (confirm('삭제 하시겠습니까 ?')) {
+    if (confirm('삭제 하시겠습니까?')) {
         xhr.onload = () => {
             if (xhr.status == 200 && xhr.readyState == 4) {
                 location.reload();
@@ -44,3 +54,4 @@ function onDelete(topicId) {
         xhr.send(null);
     } 
 };
+
